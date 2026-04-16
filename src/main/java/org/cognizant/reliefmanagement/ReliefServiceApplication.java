9732c9776f150ab42a70f40d19f5b0e5e1083118
k-mohan-cts <kommindala.mohan@cognizant.com>
2026-04-16 16:49:52 +0530

    update file strucutre with appropiate names

--- src/main/java/org/cognizant/reliefmanagement/ReliefServiceApplication.java
+++ src/main/java/org/cognizant/reliefmanagement/ReliefServiceApplication.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class ReliefManagementApplication {
+public class ReliefServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReliefManagementApplication.class, args);
+        SpringApplication.run(ReliefServiceApplication.class, args);
     }
 
 
